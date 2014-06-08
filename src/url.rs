445a545457e2b69fc4d301a9ec82e0ff59a1ee49
Jacob Mick <anon.biohacker@gmail.com>
2014-06-08 00:22:52 -0500

    unescape is partially complete

--- src/url.rs
+++ src/url.rs
@@ -2,35 +2,40 @@
   RFC 3986 Compliant URL parsing
 
   This is a port of Go's Url library.
-      -http://golang.org/src/pkg/net/url/url.go
+    -http://golang.org/src/pkg/net/url/url.go
 
   From http://en.wikipedia.org/wiki/URI_scheme#Examples:
 
-     foo://username:password@example.com:8042/over/there/index.dtb?type=animal&name=narwhal#nose
-     \_/   \_______________/ \_________/ \__/            \___/ \_/ \______________________/ \__/
-      |           |               |       |                |    |            |                |
-      |       userinfo         hostname  port              |    |          query          fragment
-      |    \________________________________/\_____________|____|/ \__/        \__/
-      |                    |                          |    |    |    |          |
-      |                    |                          |    |    |    |          |
-   scheme              authority                    path   |    |    interpretable as keys
-    name   \_______________________________________________|____|/       \____/     \_____/
-      |                         |                          |    |          |           |
-      |                 hierarchical part                  |    |    interpretable as values
-      |                                                    |    |
-      |            path               interpretable as filename |
-      |   ___________|____________                              |
-     / \ /                        \                             |
-     urn:example:animal:ferret:nose               interpretable as extension
+  foo://username:password@example.com:8042/over/there/index.dtb?type=animal&name=narwhal#nose
+  \_/   \_______________/ \_________/ \__/            \___/ \_/ \______________________/ \__/
+  |           |               |       |                |    |            |                |
+  |       userinfo         hostname  port              |    |          query          fragment
+  |    \________________________________/\_____________|____|/ \__/        \__/
+  |                    |                          |    |    |    |          |
+  |                    |                          |    |    |    |          |
+  scheme              authority                    path   |    |    interpretable as keys
+  name   \_______________________________________________|____|/       \____/     \_____/
+  |                         |                          |    |          |           |
+  |                 hierarchical part                  |    |    interpretable as values
+  |                                                    |    |
+  |            path               interpretable as filename |
+  |   ___________|____________                              |
+  / \ /                        \                             |
+  urn:example:animal:ferret:nose               interpretable as extension
 
 
 */
 
 use std::fmt;
+use std::str;
 
 /// Error codes for Url parsing.
 pub enum ErrorCode {
     /// Invalid url escape.
+    /// TODO: impl
+    /// func (e EscapeError) Error() string {
+    ///     return "invalid URL escape " + strconv.Quote(string(e))
+    /// }
     EscapeError
 }
 
@@ -67,6 +72,7 @@ impl<'a> Error<'a> {
 }
 
 /// Encoding is used to determine if a reserved error can appear unescaped.
+#[deriving(PartialEq)]
 pub enum Encoding {
     /// Paths
     EncodePath,
@@ -89,24 +95,21 @@ pub fn should_escape(c: u8, mode: Encoding) -> bool {
              'a' as u8 <= d && d <= 'z' as u8 ||
              'A' as u8 <= d && d <= 'Z' as u8  => false,
 
-        d if d == '-' as u8 ||
-             d == '_' as u8 ||
-             d == '.' as u8 ||
-             d == '~' as u8                    => false,
+            d if bytes!("-_.~").contains(&d)   => false,
 
-        d if bytes!("$&+,/:;=?@").contains(&d) => match mode {
-            EncodePath           => c == '?' as u8,
+            d if bytes!("$&+,/:;=?@").contains(&d) => match mode {
+                EncodePath           => c == '?' as u8,
 
-            EncodeUserPassword   => c == '@' as u8 ||
-                                    c == '/' as u8 ||
-                                    c == ':' as u8,
+                EncodeUserPassword   => c == '@' as u8 ||
+                                        c == '/' as u8 ||
+                                        c == ':' as u8,
 
-            EncodeQueryComponent => true,
+                EncodeQueryComponent => true,
 
-            EncodeFragment       => false
-        },
+                EncodeFragment       => false
+            },
 
-        _                                      => true
+            _                                    => true
     }        
 }
 
@@ -121,20 +124,72 @@ pub fn ishex(c: u8) -> bool {
     }
 }
 
-/// lolwut
 /// TODO: Rewrite this function with b'0'... when the feature arrives.
 pub fn unhex(c: u8) -> u8 {
     return match c {
-       d if '0' as u8 <= d && d <= '9' as u8 => c - '0' as u8,  
-       d if 'a' as u8 <= d && d <= 'f' as u8 => c - 'a' as u8 + 10,
-       d if 'A' as u8 <= d && d <= 'F' as u8 => c - 'A' as u8 + 10,
-       _                                     => 0
+        d if '0' as u8 <= d && d <= '9' as u8 => c - '0' as u8,  
+        d if 'a' as u8 <= d && d <= 'f' as u8 => c - 'a' as u8 + 10,
+        d if 'A' as u8 <= d && d <= 'F' as u8 => c - 'A' as u8 + 10,
+        _                                     => 0
     }
 }
 
+
+/// unescape unescapes a string, the mode specifics
+/// which section of the URL string is being unescaped
+/// TODO: Rewrite this function with b'0'... when the feature arrives.
+/// TODO: Refactor this beast.
+/// TODO: Ask the IRC is from_utf(s.into_bytes()).unwrap() will never fail.
+pub fn unescape(s: String, mode: Encoding) -> Result<String, String> {
+    let mut n = 0;
+    let mut has_plus = false;
+    let mut i = 0;
+    let s_slice = s.as_slice().clone();
+    let s_len = s_slice.len();
+    while i < s_len {
+        match s_slice[i] {
+            // '%' as u8
+            37 => {
+                n += 1;
+                if i + 2 >= s_len || !ishex(s_slice[i + 1]) || !ishex(s_slice[i + 2]) {
+                    let output = match s_len {
+                        q if q - i > 3 => s_slice.slice(i, i + 3),
+                        _              => s_slice.slice(i, s_len)
+                    };
+                    return Err(error_str(EscapeError)
+                               .to_string()
+                               .append(output));
+                }
+                i += 3;
+            },
+
+            // '+' as u8
+               43 => {
+                   if mode == EncodeQueryComponent {
+                       has_plus = true;
+                   }
+                   i += 1;
+               },
+
+               _  => {
+                   i += 1;
+               }
+        }
+    }
+
+    if n == 0 && !has_plus {
+        return Ok(s_slice.to_string());
+    }
+
+
+    return Ok(s_slice.to_string());
+}
+
+
 #[cfg(test)]
 mod tests {
-    use super::{ishex, should_escape, Encoding, EncodePath};
+    use super::{ishex, should_escape, EncodePath, EncodeUserPassword,
+    EncodeQueryComponent};
 
     #[test]
     fn test_ishhex() {
@@ -147,5 +202,19 @@ mod tests {
         for i in should_fail.iter() {
             assert_eq!(ishex(*i), false);
         }
+    }
+
+    #[test]
+    fn test_should_escape() {
+        assert_eq!(should_escape('z' as u8, EncodePath),           false);
+        assert_eq!(should_escape('-' as u8, EncodePath),           false);
+        assert_eq!(should_escape('?' as u8, EncodePath),           true);
+        assert_eq!(should_escape('?' as u8, EncodeUserPassword),   false);
+        assert_eq!(should_escape('?' as u8, EncodeQueryComponent), true);
+    }
+    
+    #[test]
+    fn test_unescaped() {
+        assert!(false);
     }
 }
